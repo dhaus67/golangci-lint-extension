5256944db18e3ac4876e348f70451ec6cc80f285
Daniel Haus <daniel.haus@re-cap.com>
2024-10-26 20:49:11 +0200

    Missing implementations for zed::Extension

--- src/golangci.rs
+++ src/golangci.rs
@@ -140,6 +140,30 @@ impl zed::Extension for GolangCiLintExtension {
             env: Default::default(),
         })
     }
+
+    fn language_server_initialization_options(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &zed_extension_api::Worktree,
+    ) -> Result<Option<zed_extension_api::serde_json::Value>> {
+        let settings = LspSettings::for_worktree(language_server_id.as_ref(), worktree)
+            .ok()
+            .and_then(|lsp_settings| lsp_settings.initialization_options.clone())
+            .unwrap_or_default();
+        Ok(Some(settings))
+    }
+
+    fn language_server_workspace_configuration(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &zed_extension_api::Worktree,
+    ) -> Result<Option<zed_extension_api::serde_json::Value>> {
+        let settings = LspSettings::for_worktree(language_server_id.as_ref(), worktree)
+            .ok()
+            .and_then(|lsp_settings| lsp_settings.initialization_options.clone())
+            .unwrap_or_default();
+        Ok(Some(settings))
+    }
 }
 
 zed::register_extension!(GolangCiLintExtension);
